7714c0a494a20510d10fe96a77b60764b72db8f6
alex hernandez <alexanderohernandez21@gmail.com>
2022-04-20 09:01:54 -0700

    added questions

--- index.js
+++ index.js
@@ -3,15 +3,46 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
+    {
+        name: 'Title',
+        message:'Enter title'
+    },{
+        name: 'Description',
+        message: 'Enter description'
+    },{
+        name: 'Installation',
+        message: 'Enter installation guide'
+    },{
+        name: 'Usage',
+        message: 'Enter information guide'
+    },{
+        name: 'Contribution',
+        message: 'Enter contribution guidlines'
+    },{
+        name: 'test',
+        message: 'Enter test instructions'
+    },{
+        type: 'list',
+        name: 'License',
+        message:'choose a license',
+        choices:[
+            'MIT','APACHE 2.0','GPL 3.0','BSD 3','None'
+        ],
+    },{
+        name:'userName',
+        message:'Enter your GitHub username',
+    },{
+        name:'email',
+        message:'Enter email adress'
+    }
 
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
    inquirer
-    .prompt([
+    .prompt(questions)
 
-   ])
 }
 writeToFile();
 // TODO: Create a function to initialize app
@@ -19,4 +50,5 @@ function init() {}
 
 // Function call to initialize app
 init();
+
 console.log(__dirname)
---
+++ user.md
@@ -0,0 +1,11 @@
+{
+	"Title": "title ",
+	"Description": "short description",
+	"Installation": "isntallation",
+	"Usage": "information",
+	"Contribution": "contribution",
+	"test": "test",
+	"License": "GPL 3.0",
+	"userName": "alec",
+	"email": "emai'"
+}
